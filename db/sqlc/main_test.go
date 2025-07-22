c1d74f2732baec67be0e6827f739052fa67cbc3d
Marko Milenovic <markomilenovic7@gmail.com>
2025-07-22 19:06:02 +0200

    Deadlock solved

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,6 +10,7 @@ import (
 
 
 var testQueries *Queries
+var testDB *sql.DB
 
 
 
@@ -20,12 +21,16 @@ const (
 )
 
 
+
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
---
+++ db/sqlc/store.go
@@ -0,0 +1,134 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+
+// Store provides all functions to execute SQL queries and transactions.
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+
+// NewStore creates a new Store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		Queries: New(db),
+		db:      db,
+	}
+}
+
+// ExecTx executes a function within a database transaction.
+func (store *Store) ExecTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+
+
+type TransferTxParams struct {	
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+
+type TransferTxResult struct {
+	FromAccount Account `json:"from_account"`
+	ToAccount   Account `json:"to_account"`
+	Transfer    Transfer `json:"transfer"`
+	FromEntry   Entry   `json:"from_entry"`
+	ToEntry     Entry   `json:"to_entry"`
+}
+
+
+// TransferTx, creates a transfer record, adds entries in both accounts, and updates the account balances atomically.
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.ExecTx(ctx, func(q *Queries) error {
+		var err error
+
+
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: sql.NullInt64{Int64: arg.FromAccountID, Valid: true},
+			Amount:    -arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: sql.NullInt64{Int64: arg.ToAccountID, Valid: true},
+			Amount:    +arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+	
+		account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+		if err != nil {
+			return err
+		}
+
+		result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.FromAccountID,
+			Balance: account1.Balance - arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
+		if err != nil {
+			return err
+		}
+
+		result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.ToAccountID,
+			Balance: account2.Balance + arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return result, err
+}
+
